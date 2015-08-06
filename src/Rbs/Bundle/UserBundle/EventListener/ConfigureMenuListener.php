abc4c3cfdb77edc4a8356073984865fe84ff7f1a
Samiul Amin <samiul@emicrograph.com>
2015-08-06 14:54:36 +0600

    Added menu icon

--- src/Rbs/Bundle/UserBundle/EventListener/ConfigureMenuListener.php
+++ src/Rbs/Bundle/UserBundle/EventListener/ConfigureMenuListener.php
@@ -19,12 +19,13 @@ class ConfigureMenuListener extends ContextAwareListener
 
             $menu->addChild('User', array('route' => ''))
                 ->setAttribute('dropdown', true)
+                ->setAttribute('icon', 'fa fa-user')
                 ->setLinkAttribute('data-hover', 'dropdown');
 
-            $menu['User']->addChild('User List', array('route' => 'users_home'));
-            $menu['User']->addChild('User Create', array('route' => 'user_create'));
-            $menu['User']->addChild('Group List', array('route' => 'fos_user_group_list'));
-            $menu['User']->addChild('Group Create', array('route' => 'fos_user_group_new'));
+            $menu['User']->addChild('User List', array('route' => 'users_home'))->setAttribute('icon', 'fa fa-user');
+            $menu['User']->addChild('User Create', array('route' => 'user_create'))->setAttribute('icon', 'fa fa-user-plus');
+            $menu['User']->addChild('Group List', array('route' => 'fos_user_group_list'))->setAttribute('icon', 'fa fa-users');
+            $menu['User']->addChild('Group Create', array('route' => 'fos_user_group_new'))->setAttribute('icon', 'fa fa-plus');
 
         return $menu;
         //}
