d4da3316b001d04e5dd9e4a81b96e57bb2e8b372
shahidul <shahidul@rightbrainsolution.com>
2015-09-15 17:39:52 +0600

    user group joint table name change

--- src/Rbs/Bundle/UserBundle/Entity/User.php
+++ src/Rbs/Bundle/UserBundle/Entity/User.php
@@ -52,7 +52,7 @@ class User extends BaseUser
 
     /**
      * @ORM\ManyToMany(targetEntity="Group", inversedBy="users")
-     * @ORM\JoinTable(name="user_user_group",
+     * @ORM\JoinTable(name="joint_user_group",
      *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
      *      inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id")}
      * )
