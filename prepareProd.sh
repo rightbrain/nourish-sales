6c0ffeb4a8681dd92f9f94c6602f0504c2ad118f
Samiul Amin <samiul@emicrograph.com>
2015-09-30 10:09:47 +0600

    Remove sudo from prepare sh script

--- prepareProd.sh
+++ prepareProd.sh
@@ -3,4 +3,4 @@ app/console assets:install --env=prod
 app/console assetic:dump --env=prod
 app/console assets:install --symlink web
 app/console fos:js-routing:dump
-sudo chmod 777 app/cache* app/logs* -R
+chmod 777 app/cache* app/logs* -R
