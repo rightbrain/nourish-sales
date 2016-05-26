f2ea3f44f054079aff8a619ce63e527889703909
shahidulhasan <er.shahidul@gmail.com>
2016-05-26 10:38:42 +0600

    readable sms functionality and html

--- src/Rbs/Bundle/SalesBundle/Controller/SmsController.php
+++ src/Rbs/Bundle/SalesBundle/Controller/SmsController.php
@@ -51,4 +51,41 @@ class SmsController extends Controller
         $query->addWhereAll($function);
         return $query->getResponse();
     }
+
+    /**
+     * @Route("/sms/readable", name="sms_readable")
+     * @Template()
+     * @JMS\Secure(roles="ROLE_ORDER_VIEW")
+     */
+    public function indexReadableAction()
+    {
+        $datatable = $this->get('rbs_erp.sales.datatable.sms');
+        $datatable->buildDatatable();
+
+        return $this->render('RbsSalesBundle:Sms:readable.html.twig', array(
+            'datatable' => $datatable
+        ));
+    }
+    
+    /**
+     * Lists all Sms entities.
+     *
+     * @Route("/readable_sms_list_ajax", name="readable_sms_list_ajax", options={"expose"=true})
+     * @Method("GET")
+     * @JMS\Secure(roles="ROLE_ORDER_VIEW")
+     */
+    public function readableSmsListAjaxAction()
+    {
+        $datatable = $this->get('rbs_erp.sales.datatable.sms');
+        $datatable->buildDatatable();
+
+        $query = $this->get('sg_datatables.query')->getQueryFrom($datatable);
+        /** @var QueryBuilder $qb */
+        $function = function($qb)
+        {
+            $qb->andWhere("sms.status = 'READ'");
+        };
+        $query->addWhereAll($function);
+        return $query->getResponse();
+    }
 }
