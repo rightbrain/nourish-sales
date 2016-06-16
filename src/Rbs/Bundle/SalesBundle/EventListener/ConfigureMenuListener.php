77027f57136c984327316743f99eeac450bc5e5c
shahidul <er.shahidul@gmail.com>
2016-06-16 20:25:32 +0600

    temp fix

--- src/Rbs/Bundle/SalesBundle/EventListener/ConfigureMenuListener.php
+++ src/Rbs/Bundle/SalesBundle/EventListener/ConfigureMenuListener.php
@@ -29,11 +29,11 @@ class ConfigureMenuListener extends ContextAwareListener
             }
         }
 
-//        if ($this->authorizationChecker->isGranted(array('ROLE_AGENT', 'ROLE_ORDER_VIEW', 'ROLE_ORDER_CREATE', 'ROLE_ORDER_EDIT', 'ROLE_ORDER_APPROVE', 'ROLE_ORDER_CANCEL'))) {
-//            $menu['Sales']->addChild('Orders From SMS', array('route' => 'order_readable_sms'))
-//                ->setAttribute('icon', 'fa fa-th-list');
-//
-//        }
+        if ($this->authorizationChecker->isGranted(array('ROLE_AGENT', 'ROLE_ORDER_VIEW', 'ROLE_ORDER_CREATE', 'ROLE_ORDER_EDIT', 'ROLE_ORDER_APPROVE', 'ROLE_ORDER_CANCEL'))) {
+            $menu['Sales']->addChild('Orders From SMS', array('route' => 'order_readable_sms'))
+                ->setAttribute('icon', 'fa fa-th-list');
+
+        }
 
         if ($this->authorizationChecker->isGranted(array('ROLE_DELIVERY_MANAGE'))) {
             $menu['Sales']->addChild('Deliveries', array('route' => 'deliveries_home'))
