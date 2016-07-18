991a574287b7d86691ba8f63186e5a2099d431cd
shahidulhasan <er.shahidul@gmail.com>
2016-07-18 14:44:07 +0600

    hide factory, factory type, area, sub category, agent because of unused

--- src/Rbs/Bundle/CoreBundle/EventListener/ConfigureMenuListener.php
+++ src/Rbs/Bundle/CoreBundle/EventListener/ConfigureMenuListener.php
@@ -47,37 +47,37 @@ class ConfigureMenuListener extends ContextAwareListener
             }
         }
 
-        if ($this->authorizationChecker->isGranted('ROLE_SUB_CATEGORY_MANAGE')) {
-            $menu['Manage System']->addChild('Sub Categories', array('route' => 'subcategory'))
-                ->setAttribute('icon', 'fa fa-th-list');
-            if ($this->isMatch('subcategory')) {
-                $menu['Manage System']->getChild('Sub Categories')->setCurrent(true);
-            }
-        }
+//        if ($this->authorizationChecker->isGranted('ROLE_SUB_CATEGORY_MANAGE')) {
+//            $menu['Manage System']->addChild('Sub Categories', array('route' => 'subcategory'))
+//                ->setAttribute('icon', 'fa fa-th-list');
+//            if ($this->isMatch('subcategory')) {
+//                $menu['Manage System']->getChild('Sub Categories')->setCurrent(true);
+//            }
+//        }
 
-        if ($this->authorizationChecker->isGranted('ROLE_AREA_MANAGE')) {
-            $menu['Manage System']->addChild('Areas', array('route' => 'area'))
-                ->setAttribute('icon', 'fa fa-map-marker');
-            if ($this->isMatch('area')) {
-                $menu['Manage System']->getChild('Areas')->setCurrent(true);
-            }
-        }
+//        if ($this->authorizationChecker->isGranted('ROLE_AREA_MANAGE')) {
+//            $menu['Manage System']->addChild('Areas', array('route' => 'area'))
+//                ->setAttribute('icon', 'fa fa-map-marker');
+//            if ($this->isMatch('area')) {
+//                $menu['Manage System']->getChild('Areas')->setCurrent(true);
+//            }
+//        }
 
-        if ($this->authorizationChecker->isGranted(array('ROLE_FACTORY_MANAGE', 'ROLE_PROJECT_MANAGE'))) {
-            $menu['Manage System']->addChild('Factories', array('route' => 'project'))
-                ->setAttribute('icon', 'fa fa-th-list');
-            if ($this->isMatch('project') && !$this->isMatch('projecttype')) {
-                $menu['Manage System']->getChild('Factories')->setCurrent(true);
-            }
-        }
+//        if ($this->authorizationChecker->isGranted(array('ROLE_FACTORY_MANAGE', 'ROLE_PROJECT_MANAGE'))) {
+//            $menu['Manage System']->addChild('Factories', array('route' => 'project'))
+//                ->setAttribute('icon', 'fa fa-th-list');
+//            if ($this->isMatch('project') && !$this->isMatch('projecttype')) {
+//                $menu['Manage System']->getChild('Factories')->setCurrent(true);
+//            }
+//        }
 
-        if ($this->authorizationChecker->isGranted(array('ROLE_PROJECT_TYPE_MANAGE', 'ROLE_FACTORY_TYPE_MANAGE'))) {
-            $menu['Manage System']->addChild('Factory Types', array('route' => 'projecttype'))
-                ->setAttribute('icon', 'fa fa-th-list');
-            if ($this->isMatch('projecttype')) {
-                $menu['Manage System']->getChild('Factory Types')->setCurrent(true);
-            }
-        }
+//        if ($this->authorizationChecker->isGranted(array('ROLE_PROJECT_TYPE_MANAGE', 'ROLE_FACTORY_TYPE_MANAGE'))) {
+//            $menu['Manage System']->addChild('Factory Types', array('route' => 'projecttype'))
+//                ->setAttribute('icon', 'fa fa-th-list');
+//            if ($this->isMatch('projecttype')) {
+//                $menu['Manage System']->getChild('Factory Types')->setCurrent(true);
+//            }
+//        }
 
         if ($this->authorizationChecker->isGranted('ROLE_DEPO_MANAGE')) {
             $menu['Manage System']->addChild('Depos', array('route' => 'depo'))
