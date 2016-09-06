1c8a6445ce2f331bea88326a031bf452c808cd06
shahidulhasan <er.shahidul@gmail.com>
2016-09-06 12:20:33 +0600

    add order number as required and order field filter by user

--- src/Rbs/Bundle/SalesBundle/Form/Type/CashReceiveForm.php
+++ src/Rbs/Bundle/SalesBundle/Form/Type/CashReceiveForm.php
@@ -2,6 +2,7 @@
 
 namespace Rbs\Bundle\SalesBundle\Form\Type;
 
+use Rbs\Bundle\SalesBundle\Entity\Order;
 use Rbs\Bundle\SalesBundle\Repository\OrderRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\DataTransformer\DateTimeToStringTransformer;
@@ -10,29 +11,64 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CashReceiveForm extends AbstractType
 {
+    private $depoId;
+
+    public function __construct($depoId)
+    {
+        $this->depoId = $depoId;
+    }
+
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('amount')
+
+        if($this->depoId != 0){
+            $builder
             ->add('orderRef', 'entity', array(
                 'class' => 'RbsSalesBundle:Order',
                 'attr' => array(
                     'class' => 'select2me'
                 ),
                 'property' => 'id',
-                'required' => false,
+                'required' => true,
                 'empty_value' => 'Select Order',
                 'empty_data' => null,
                 'query_builder' => function (OrderRepository $repository)
                 {
                     return $repository->createQueryBuilder('o')
-                        ->join('o.agent', 'a');
+                        ->join('o.agent', 'a')
+                        ->join('o.deliveries', 'deliveries')
+                        ->join('deliveries.depo', 'd')
+                        ->where('o.orderState != :CANCEL')
+                        ->andWhere('d.id = :depoId')
+                        ->setParameters(array('CANCEL'=>Order::ORDER_STATE_CANCEL, 'depoId'=>$this->depoId));
                 }
-            ))
+            ));
+        }else{
+            $builder
+            ->add('orderRef', 'entity', array(
+                'class' => 'RbsSalesBundle:Order',
+                'attr' => array(
+                    'class' => 'select2me'
+                ),
+                'property' => 'id',
+                'required' => true,
+                'empty_value' => 'Select Order',
+                'empty_data' => null,
+                'query_builder' => function (OrderRepository $repository)
+                {
+                    return $repository->createQueryBuilder('o')
+                        ->join('o.agent', 'a')
+                        ->where('o.orderState != :CANCEL')
+                        ->setParameters(array('CANCEL'=>Order::ORDER_STATE_CANCEL));
+                }
+            ));
+        }
+        $builder
+            ->add('amount')
             ->add('depositor')
             ->add('remark')
             ->add('submit', 'submit', array(
