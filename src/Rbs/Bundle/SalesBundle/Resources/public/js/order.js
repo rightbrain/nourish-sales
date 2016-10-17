5e23d2765c35c024491e0639749332c5fb6f99ce
shahidulhasan <er.shahidul@gmail.com>
2016-10-17 17:28:48 +0600

    order list delivery state filter remove hold and add ready

--- src/Rbs/Bundle/SalesBundle/Resources/public/js/order.js
+++ src/Rbs/Bundle/SalesBundle/Resources/public/js/order.js
@@ -215,7 +215,7 @@ var Order = function()
                 },
                 {
                     column_number: 5,
-                    data: ["PENDING", "PARTIALLY_SHIPPED", "SHIPPED", "HOLD"],
+                    data: ["PENDING", "PARTIALLY_SHIPPED", "SHIPPED", "READY"],
                     filter_container_id: "order-delivery-status",
                     filter_reset_button_text: false,
                     filter_default_label: "Delivery State"
