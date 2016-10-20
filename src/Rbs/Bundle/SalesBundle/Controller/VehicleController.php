2d31db8293fc1ba7edeeb924c9723f1c8c8069ba
shahidulhasan <er.shahidul@gmail.com>
2016-10-20 11:48:53 +0600

    vehicle set list desc

--- src/Rbs/Bundle/SalesBundle/Controller/VehicleController.php
+++ src/Rbs/Bundle/SalesBundle/Controller/VehicleController.php
@@ -234,12 +234,14 @@ class VehicleController extends BaseController
                 $qb->andWhere('sales_vehicles.vehicleIn IS NOT NULL');
                 $qb->andWhere('sales_vehicles.vehicleOut IS NULL');
                 $qb->andWhere('sales_vehicles.deliveries IS NULL');
+                $qb->orderBy('sales_vehicles.createdAt' ,'DESC');
             }else{
                 $qb->join('sales_vehicles.depo', 'd');
                 $qb->andWhere('d.id =:depoId');
                 $qb->andWhere('sales_vehicles.vehicleIn IS NOT NULL');
                 $qb->andWhere('sales_vehicles.vehicleOut IS NULL');
                 $qb->andWhere('sales_vehicles.deliveries IS NULL');
+                $qb->orderBy('sales_vehicles.createdAt' ,'DESC');
                 $qb->setParameters(array('depoId'=>$depoId));
             }
         };
