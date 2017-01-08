30dff65c37416ba507779cfa35678713de96abc6
Samiul Amin <iamshanto@gmail.com>
2017-01-08 17:06:15 +0600

    1587

--- src/Rbs/Bundle/SalesBundle/Form/Type/AgentUpdateForm.php
+++ src/Rbs/Bundle/SalesBundle/Form/Type/AgentUpdateForm.php
@@ -25,78 +25,102 @@ class AgentUpdateForm extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $agentId = $builder->getData()->getAgentID();
+        $isAgentIdDisabled = !empty($agentId);
         $builder
             ->add('VIP', 'choice', array(
-                'choices'  => array(
-                    '0' => 'Not',
-                    '1' => 'Yes'
+                    'choices' => array(
+                        '0' => 'No',
+                        '1' => 'Yes',
+                    ),
+                    'label' => 'VIP'
                 )
-            ))
-            ->add('itemType', 'entity', array(
-                'class' => 'RbsCoreBundle:ItemType',
-                'label' => 'Item Type',
-                'property' => 'itemType',
-                'required' => false,
-                'empty_value' => 'Select Item Type',
-                'empty_data' => null,
-                'query_builder' => function (ItemTypeRepository $repository)
-                {
-                    return $repository->createQueryBuilder('it')
-                        ->where('it.deletedAt IS NULL')
-                        ->orderBy('it.itemType','ASC');
-                }
-            ))
-            ->add('agentID');
-
-    if(!$this->openingBalanceFlag){
-        $builder
-            ->add('openingBalance')
-            ->add('openingBalanceType', 'choice', array(
-                'choices'  => array(
-                    'CR' => Agent::CR,
-                    'DR' => Agent::DR
+            )
+            ->add(
+                'itemType',
+                'entity',
+                array(
+                    'class'         => 'RbsCoreBundle:ItemType',
+                    'label'         => 'Item Type',
+                    'property'      => 'itemType',
+                    'required'      => false,
+                    'empty_value'   => 'Select Item Type',
+                    'empty_data'    => null,
+                    'query_builder' => function (ItemTypeRepository $repository) {
+                        return $repository->createQueryBuilder('it')
+                            ->where('it.deletedAt IS NULL')
+                            ->orderBy('it.itemType', 'ASC');
+                    },
                 )
-            ));
-    }
+            )
+            ->add(
+                'agentID', null, array(
+                    'label' => 'Agent ID',
+                    'disabled' => $isAgentIdDisabled
+                )
+            );
+
+        if (!$this->openingBalanceFlag) {
+            $builder
+                ->add('openingBalance')
+                ->add(
+                    'openingBalanceType',
+                    'choice',
+                    array(
+                        'choices' => array(
+                            'CR' => Agent::CR,
+                            'DR' => Agent::DR,
+                        ),
+                    )
+                );
+        }
 
         $builder
-            ->add('depo', 'entity', array(
-                'class' => 'RbsCoreBundle:Depo',
-                'property' => 'name',
-                'required' => true,
-                'empty_value' => 'Select Depo',
-                'empty_data' => null,
-                'query_builder' => function (DepoRepository $repository)
-                {
-                    return $repository->createQueryBuilder('w')
-                        ->where('w.deletedAt IS NULL')
-                        ->orderBy('w.name','ASC');
-                }
-            ))
-            ->add('agentGroup', 'entity', array(
-                'class' => 'RbsSalesBundle:AgentGroup',
-                'label' => 'Agent Group',
-                'property' => 'label',
-                'required' => false,
-                'empty_value' => 'Select Group',
-                'empty_data' => null,
-                'query_builder' => function (AgentGroupRepository $repository)
-                {
-                    return $repository->createQueryBuilder('cg')
-                        ->where('cg.deletedAt IS NULL')
-                        ->orderBy('cg.label','ASC');
-                }
-            ))
-        ;
+            ->add(
+                'depo',
+                'entity',
+                array(
+                    'class'         => 'RbsCoreBundle:Depo',
+                    'property'      => 'name',
+                    'required'      => true,
+                    'empty_value'   => 'Select Depo',
+                    'empty_data'    => null,
+                    'query_builder' => function (DepoRepository $repository) {
+                        return $repository->createQueryBuilder('w')
+                            ->where('w.deletedAt IS NULL')
+                            ->orderBy('w.name', 'ASC');
+                    },
+                )
+            )
+            ->add(
+                'agentGroup',
+                'entity',
+                array(
+                    'class'         => 'RbsSalesBundle:AgentGroup',
+                    'label'         => 'Agent Group',
+                    'property'      => 'label',
+                    'required'      => false,
+                    'empty_value'   => 'Select Group',
+                    'empty_data'    => null,
+                    'query_builder' => function (AgentGroupRepository $repository) {
+                        return $repository->createQueryBuilder('cg')
+                            ->where('cg.deletedAt IS NULL')
+                            ->orderBy('cg.label', 'ASC');
+                    },
+                )
+            );
 
         $builder
             ->add('user', new UserAgentUpdateForm());
 
         $builder
-            ->add('submit', 'submit', array(
-                'attr'     => array('class' => 'btn green')
-            ))
-        ;
+            ->add(
+                'submit',
+                'submit',
+                array(
+                    'attr' => array('class' => 'btn green'),
+                )
+            );
     }
 
     /**
