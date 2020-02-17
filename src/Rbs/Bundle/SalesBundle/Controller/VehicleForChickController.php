fc4c88ec5497fed0083ad5f0897373bb1640a3db
rakeybulhasan <rakeybulhasan@emicrograph.com>
2020-02-17 20:29:38 +0600

    user role separate for chick and feed

--- src/Rbs/Bundle/SalesBundle/Controller/VehicleForChickController.php
+++ src/Rbs/Bundle/SalesBundle/Controller/VehicleForChickController.php
@@ -60,7 +60,7 @@ class VehicleForChickController extends BaseController
             $qb->join("sales_vehicles.depo", "d");
             $qb->andWhere('d.depotType = :type');
             $qb->setParameter('type',Depo::DEPOT_TYPE_CHICK);
-            if(!in_array('ROLE_SUPER_ADMIN', $user->getRoles())||!in_array('ROLE_CHICK_ORDER_MANAGE', $user->getRoles())){
+            if(!in_array('ROLE_ADMIN', $user->getRoles())){
                 $qb->join("d.users", "u");
                 $qb->andWhere("u = :user");
                 $qb->setParameter('user', $user);
