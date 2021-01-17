b7a1831acd40f97594fb05601830cce91adc22f7
rakeybulhasan <rakeybulhasan@gmail.com>
2021-01-18 00:55:44 +0600

    order item temp insert raw query done

--- src/Rbs/Bundle/SalesBundle/Controller/ChickOrderController.php
+++ src/Rbs/Bundle/SalesBundle/Controller/ChickOrderController.php
@@ -649,8 +649,15 @@ WHERE a.agent_type = 'CHICK' AND u.deleted_at IS NULL AND l.id IN ({$areaId})";
                   foreach ($addedTempOrders as $order){
 
                       $orderObj = $this->getDoctrine()->getRepository('RbsSalesBundle:OrderChickTemp')->find($order['id']);
+                      $locationDist = $orderObj->getAgent()->getUser()->getZilla()->getId();
+                      $sqlChild ="INSERT INTO sales_order_items_chick_temp
+    (`order_id`, `item_id`,`quantity`, `price`, `mrp_price`, `total_amount`)
+SELECT {$order['id']}, core_items.id, 0, (SELECT core_item_price.price FROM `core_item_price` WHERE `item_id` = core_items.id AND `location_id` = {$locationDist} AND `is_active` = 1) as price, (SELECT core_item_price.mrp_price FROM `core_item_price` WHERE `item_id` = core_items.id AND `location_id` = {$locationDist} AND `is_active` = 1) AS mrpPrice,0 FROM `core_items` WHERE `item_types` = 3";
 
-                      $this->insertOrderItem($orderObj, $chickItems);
+                      $qb = $em->getConnection()->prepare($sqlChild);
+                      $qb->execute();
+
+//                      $this->insertOrderItem($orderObj, $chickItems);
                   }
                }
             $addedData = $this->getTempChickOrdersByDateDepot($date, $depo);
