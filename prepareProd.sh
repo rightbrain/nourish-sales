64aec7457395f4d87cb034e83a3221af5a4d2831
Rakeybul Hasan <rakeybul.rbs@gmail.com>
2026-01-18 14:28:58 +0600

    challan print

--- prepareProd.sh
+++ prepareProd.sh
@@ -2,4 +2,4 @@ app/console cache:clear --env=prod
 app/console assets:install --env=prod --symlink web
 app/console assetic:dump --env=prod
 app/console fos:js-routing:dump
-chmod 777 app/cache* app/logs* -R
+sudo chmod 777 app/cache* app/logs* -R
